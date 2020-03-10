92e9c30db051e312c96ecd75a30ae6bffd6c4b51
BETARI Oualid <beta.oualid@gmail.com>
2020-03-10 22:16:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    $acceptLang = ['fr', 'en'];
+    $lang = in_array($lang, $acceptLang) ? $lang : 'en';
+	require_once "index_{$lang}.php"; 
+
+?>
