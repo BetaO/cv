122a187b4100f23a2b57aec2fbf14029fcb588f8
BETARI Oualid <beta.oualid@gmail.com>
2020-03-10 22:09:12 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    $acceptLang = ['fr', 'en'];
-    $lang = in_array($lang, $acceptLang) ? $lang : 'en';
-	require_once "index_{$lang}.php"; 
-
-?>
